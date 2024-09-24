c5ab69c30375fc493076c6a30803debefcd44883
benja <benjamcp@gmail.com>
2024-09-24 18:10:23 -0300

    boton_ingresar_asignatura_arreglado_v1

--- src/app/ingresar-asignatura/ingresar-asignatura.page.ts
+++ src/app/ingresar-asignatura/ingresar-asignatura.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-ingresar-asignatura',
   templateUrl: './ingresar-asignatura.page.html',
@@ -15,27 +16,45 @@ export class IngresarAsignaturaPage implements OnInit {
   asignatura: string = '';
   asignaturaValida: boolean = true;
   mensajeError: string = '';
-  constructor(private router: Router) { }
+  constructor(private router: Router, private alertController: AlertController)  { }
 
   ngOnInit() {
   }
   validarCodigo() {
-    if (this.codigo && this.codigo.toString().length === 5) {
-      if (/^\d+$/.test(this.codigo)) {
+    // Verificamos que el código no sea nulo y tenga exactamente 4 caracteres
+    if (this.codigo && this.codigo.toString().length === 4) {
+      // Utilizamos una expresión regular para validar el formato
+      const regex = /^(?=(?:[^0-9]*[0-9]){3})(?=(?:[^a-zA-Z]*[a-zA-Z]){1}).{4}$/;
+      if (regex.test(this.codigo)) {
         this.codigoValido = true;
         this.mensajeError = '';
       } else {
         this.codigoValido = false;
-        this.mensajeError = 'No se permiten caracteres.';
+        this.mensajeError = 'El código debe contener exactamente 3 números y 1 letra.';
       }
     } else {
       this.codigoValido = false;
-      this.mensajeError = 'El código debe tener exactamente 5 dígitos.';
+      this.mensajeError = 'El código debe tener exactamente 4 caracteres.';
     }
   }
   validarAsignatura() {
     const length = this.asignatura.trim().length;
     this.asignaturaValida = length >= 3 && length <= 20;
+  }
+  async submitCodigo() {
+    this.validarCodigo(); // Valida el código antes de proceder
+    if (this.codigoValido) {
+      const alert = await this.alertController.create({
+        header: 'Advertencia',
+        subHeader: 'Nuevo en Ionic',
+        message: '¿Está seguro de TERMINAR de pasar asistencia?',
+        buttons: this.alertButtons,
+        backdropDismiss: false
+      });
+      await alert.present();
+    } else {
+      console.log("Código no válido.");
+    }
   }
 
   alertButtons_2=[{
