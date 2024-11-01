ac0ae51d0ec53b19cc55758e7c1f40ce2f5d72c4
matiascolomer4 <matiascolomer4@gmail.com>
2024-11-01 21:47:50 +0000

    Implementación Firebase

--- src/app/login/login/login.page.ts
+++ src/app/login/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,19 +12,32 @@ export class LoginPage {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService, private toastController: ToastController) {}
 
   login() {
-    // Validar el correo electrónico para determinar si es alumno o profesor
-    if (this.email.endsWith('@alumno.cl')) {
-      // Redirigir a menú estudiante
-      this.router.navigate(['/menu-estudiante']);
-    } else if (this.email.endsWith('@profesor.cl')) {
-      // Redirigir a menú profesor
-      this.router.navigate(['/menu-profesor']);
-    } else {
-      // Mostrar un mensaje de error si el correo no es válido
-      console.error('Correo no válido');
-    }
+    this.authService.login(this.email, this.password)
+      .then(() => {
+        if (this.email.endsWith('@alumno.cl')) {
+          this.router.navigate(['/menu-estudiante']);
+          this.presentToast('Inicio de sesión exitoso como estudiante.');
+        } else if (this.email.endsWith('@profesor.cl')) {
+          this.router.navigate(['/menu-profesor']);
+          this.presentToast('Inicio de sesión exitoso como profesor.');
+        } else {
+          this.presentToast('Correo no válido.');
+        }
+      })
+      .catch(error => {
+        console.error('Error en el login:', error);
+        this.presentToast('Error en el inicio de sesión. Inténtalo de nuevo.');
+      });
+  }
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'top'
+    });
+    await toast.present();
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD8eWIi-tqS6pUQbtGY7UaLhvA94nxjSsQ",
+  authDomain: "programacion-movil-6fcf8.firebaseapp.com",
+  projectId: "programacion-movil-6fcf8",
+  storageBucket: "programacion-movil-6fcf8.firebasestorage.app",
+  messagingSenderId: "4116629945",
+  appId: "1:4116629945:web:11d275e533cbb6cab64e68",
+  measurementId: "G-33G4JLHP52"
+  }
 };
 
-/*
+/*s
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
