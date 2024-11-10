d46cbb6066fa8696874285cf0c8bbcc103eb2aee
matiascolomer4 <matiascolomer4@gmail.com>
2024-11-10 20:34:38 +0000

    commit xd

--- src/app/asistencia/asistencia.page.ts
+++ src/app/asistencia/asistencia.page.ts
@@ -1,48 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 @Component({
   selector: 'app-asistencia',
   templateUrl: './asistencia.page.html',
   styleUrls: ['./asistencia.page.scss'],
 })
 export class AsistenciaPage implements OnInit {
-  accion:string=''
-  hola:string='QR EJEMPLO'
-  clase:string=''
+  accion: string = '';
+  hola: string = 'QR EJEMPLO';
+  clase: string = '';
 
-  accion_1: string=''//cuidado con la reutilizacion de codigo, con la parte del hadler
-  alertButtons_1=[{
-    text:'Aceptar',
-    cssClass:'btnAceptarStyle',
-    handler:()=>{
-      this.accion_1='Presionó aceptar';
-      this.router.navigate(['/menu-profesor']);
-    }
-  },{
-    text:'Cancelar',
-    cssClass:'btnCancelarStyle',
-    handler:()=>{
-      this.accion_1="Presionó cancelar";
-    }
-  }]
-  alertButtons_2=[{
-    text:'Aceptar',
-    cssClass:'btnAceptarStyle',
-    handler:()=>{
-      this.accion='Presionó aceptar';
-      this.router.navigate(['/menu-profesor']);
-    }
-  },{
-    text:'Cancelar',
-    cssClass:'btnCancelarStyle',
-    handler:()=>{
-      this.accion="Presionó cancelar";
-    }
-  }]
+  // Botones de la alerta para "Terminar asistencia"
+  alertButtons_1 = [
+    {
+      text: 'Aceptar',
+      cssClass: 'btnAceptarStyle',
+      handler: () => {
+        this.accion = 'Presionó aceptar';
+        this.router.navigate(['/menu-profesor']);
+      },
+    },
+    {
+      text: 'Cancelar',
+      cssClass: 'btnCancelarStyle',
+      handler: () => {
+        this.accion = 'Presionó cancelar';
+      },
+    },
+  ];
 
-  constructor(private router: Router) { }
+  // Botones de la alerta para "Cancelar asistencia"
+  alertButtons_2 = [
+    {
+      text: 'Aceptar',
+      cssClass: 'btnAceptarStyle',
+      handler: () => {
+        this.accion = 'Presionó aceptar';
+        this.router.navigate(['/menu-profesor']);
+      },
+    },
+    {
+      text: 'Cancelar',
+      cssClass: 'btnCancelarStyle',
+      handler: () => {
+        this.accion = 'Presionó cancelar';
+      },
+    },
+  ];
 
-  ngOnInit() {
+  constructor(private router: Router, private firestore: AngularFirestore) {}
+
+  ngOnInit() {}
+
+  async generateQR() {
+    // Lógica para generar el QR
   }
-
 }
--- src/app/menu-qr/menu-qr.page.ts
+++ src/app/menu-qr/menu-qr.page.ts
@@ -1,34 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { ItemList } from 'src/app/interfaces/itemlist';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { getAuth } from 'firebase/auth';
+
+interface Ramo {
+  titulo: string;
+  UIDProfesor: string;
+}
+
 @Component({
   selector: 'app-menu-qr',
   templateUrl: './menu-qr.page.html',
   styleUrls: ['./menu-qr.page.scss'],
 })
 export class MenuQRPage implements OnInit {
+  vinculos: { ruta: string; titulo: string; icono: string }[] = [];
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) {}
 
   ngOnInit() {
+    this.loadRamos();
   }
-  vinculos:ItemList[]=[{
-    ruta:'/asistencia',
-    titulo:'Asignatura_1',
-    icono:''
-  },
-    {
-      ruta:'/asistencia',
-      titulo:'Asignatura_2',
-      icono:''
 
-  },
-  {
-    ruta:'/asistencia',
-    titulo:'Asignatura_3',
-    icono:''
+  async loadRamos() {
+    const auth = getAuth();
+    const user = auth.currentUser;
 
-},
+    if (user) {
+      const uidProfesor = user.uid;
 
-  ];
-  
+      // Realizar una consulta para obtener los ramos con UIDProfesor coincidente
+      const ramosSnapshot = await this.firestore
+        .collection<Ramo>('Ramos', (ref) => ref.where('UIDProfesor', '==', uidProfesor))
+        .get()
+        .toPromise();
+
+      this.vinculos = ramosSnapshot.docs.map((doc) => ({
+        ruta: '/asistencia',
+        titulo: doc.id,  // Usa el ID del documento como título
+        icono: 'book-outline',
+      }));
+    } else {
+      console.error('No hay usuario autenticado');
+    }
+  }
 }
